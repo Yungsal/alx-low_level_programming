42a050025860a5c87fda1a3e50f14a58f02cc831
Yungsal <salyung0@gmail.com>
2023-08-24 18:57:48 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[1]);
+	}
+	_putchar('\n');
+	return (0);
+}
