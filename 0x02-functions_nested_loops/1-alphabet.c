9a73799ceb71f508f4f2b4ae922c91b0048b3c90
Yungsal <salyung0@gmail.com>
2023-08-29 08:12:27 -0700

    function that prints out alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets
+ *
+ */
+void print_alphabet(void)
+{
+	char al;
+	
+	al ='a'
+	while (al <= 'z')
+	{
+	__putchar(al);
+	}
+	_putchar('\n');
+}
